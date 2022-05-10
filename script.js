79c06cd1590482e1a1463618bb025bf44dee6884
Cristian Vargas <cristian.v0223@gmail.com>
2022-05-09 20:55:15 -0500

    added starting variables in javascript for the search form, input, overview results container, search history results container, and five day forecast container

---
+++ script.js
@@ -0,0 +1,10 @@
+let searchForm = document.querySelectorAll("#search-form");
+let searchInput = document.querySelectorAll("#search-input");
+let overviewContainer = document.querySelectorAll(".overview-container");
+let historyContainer = document.querySelectorAll(".search-history");
+let fiveDayForecast = document.querySelectorAll(".five-day-forecast");
+
+
+
+
+searchForm.addEventListener('submit', formSubmitHandler);
