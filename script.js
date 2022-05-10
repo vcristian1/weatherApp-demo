626db677f8396914a4a6091f860adba6daa57b18
Cristian Vargas <cristian.v0223@gmail.com>
2022-05-09 22:19:08 -0500

    added js for formSubmitHandler function, showFiveDayForecast function, and the window.onload function to show the overview

--- script.js
+++ script.js
@@ -3,8 +3,41 @@ let searchInput = document.querySelectorAll("#search-input");
 let overviewContainer = document.querySelectorAll(".overview-container");
 let historyContainer = document.querySelectorAll(".search-history");
 let fiveDayForecast = document.querySelectorAll(".five-day-forecast");
-let APIKey = "1644f4ee3b0c47f3bf57fd902100edcc";
-let APICall = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ${APIkey}
-  
+let city = searchInput.value
 
-searchForm.addEventListener('submit', formSubmitHandler);
+let formSubmitHandler = function (event) {
+    let city = searchInput.value.trim();
+  
+    if (city) {
+      showFiveDayForecast(city);
+  
+      fiveDayForecast.textContent = '';
+      searchInput.value = '';
+    } else {
+      alert('Please enter a valid city name');
+    }
+  };
+
+function showFiveDayForecast(city) {
+    let key = 'fc4d1682ddb59485eb333396c62bf9ea';
+    //console saying APIKey is invalid.
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key
+   
+    fetch(apiUrl)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      console.log(data);
+    })
+    .catch(function() {
+      // catch any errors
+    });
+}
+  
+window.onload = function() {
+    let APIKey = "fc4d1682ddb59485eb333396c62bf9ea";
+    showFiveDayForecast(APIKey);
+  }
+
+
+
+$(searchForm).submit(showFiveDayForecast);
