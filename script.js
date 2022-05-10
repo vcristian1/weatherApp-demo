fdb661a9b3541db5ee1d47563bd020b09f355bae
Cristian Vargas <cristian.v0223@gmail.com>
2022-05-09 21:17:47 -0500

    linked script.js and jquery files in HTML

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let overviewContainer = document.querySelectorAll(".overview-container");
 let historyContainer = document.querySelectorAll(".search-history");
 let fiveDayForecast = document.querySelectorAll(".five-day-forecast");
 let APIKey = "1644f4ee3b0c47f3bf57fd902100edcc";
-
+let APICall = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ${APIkey}
   
 
 searchForm.addEventListener('submit', formSubmitHandler);
