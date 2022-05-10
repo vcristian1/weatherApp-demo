dfe5f39e6962b1aa2952837649d4d9a755906ea5
Cristian Vargas <cristian.v0223@gmail.com>
2022-05-09 21:09:22 -0500

    added API Key to JS file

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let searchInput = document.querySelectorAll("#search-input");
 let overviewContainer = document.querySelectorAll(".overview-container");
 let historyContainer = document.querySelectorAll(".search-history");
 let fiveDayForecast = document.querySelectorAll(".five-day-forecast");
+let APIKey = "1644f4ee3b0c47f3bf57fd902100edcc";
 
-
-
+  
 
 searchForm.addEventListener('submit', formSubmitHandler);
