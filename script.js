bf7a6dbe784554305faa264f2f210c3f96ddfe1d
Cristian Vargas <cristian.v0223@gmail.com>
2022-05-11 19:15:25 -0500

    fixed js to pull overview data from weather API on click on search button

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let searchForm = document.querySelectorAll("#search-form");
-let searchInput = document.querySelectorAll("#search-input");
-let overviewContainer = document.querySelectorAll(".overview-container");
-let historyContainer = document.querySelectorAll(".search-history");
-let fiveDayForecast = document.querySelectorAll(".five-day-forecast");
-var city = "Chicago"
+var searchForm = document.getElementById("search-form");
+var searchInput = document.getElementById("search-input");
+var overviewContainer = document.getElementById("overview-container");
+var historyContainer = document.getElementById("search-history");
+var fiveDayForecast = document.getElementById("five-day-forecast");
+var searchButton = document.getElementById("search-button");
+var city = searchInput
 
 
-function showFiveDayForecast() {
+
+function currentForecast(event) {
+    event.preventDefault();
     var APIkey = '63e195e07e4897eb45e421f76c43b1dd';
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
-   
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${APIkey}`;
+
     fetch(apiUrl)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
@@ -21,6 +24,11 @@ function showFiveDayForecast() {
     });
 }
 
+function getWeather () {
+    currentForecast()
+    
+}
+
 function drawWeather( d ) {
 	var fahrenheight = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
 	
@@ -29,9 +37,13 @@ function drawWeather( d ) {
 	document.getElementById('temp').innerHTML = "Temp: " + fahrenheight + '&deg;';
     document.getElementById('humidity').innerHTML = "Humidity: " + d.main.humidity + '%';
 }
-  
-window.onload = function() {
-    showFiveDayForecast();
-}
+var testButton = document.getElementById("test-button");
 
-$("#searchForm").submit(showFiveDayForecast);
+searchButton.addEventListener("click", currentForecast);
+
+// window.onload = function() {
+//     showFiveDayForecast();
+// }
+
+// create a click event where on the submit of the button, it runs the showFiveDayForecast function
+// and saves the input value to the variable city before the function runs?
