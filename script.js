a1c96fb77c43a994b028b62cee37e6cd3db37d34
Cristian Vargas <cristian.v0223@gmail.com>
2022-05-13 22:17:13 -0500

    added code to script.js,index.html, and style.css. Overview box is styled similar to the Five Day Forecast. Five Day Forecast on click shows 5 boxes with data but does not populate the data from the API for days 2-5. Search History shows the input value but doesnt save and print the next input to the page

--- script.js
+++ script.js
@@ -7,17 +7,26 @@ var searchButton = document.getElementById("search-button");
 var city = searchInput
 var APIkey = '63e195e07e4897eb45e421f76c43b1dd';
 
+function displayDate() {
+  $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+}
+setInterval(displayDate, 100);
+
 
 
 function currentForecast(event) {
     var apiUrl1 = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${APIkey}`;
+    var apiUrl2 = `https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=${APIkey}`;
 
     fetch(apiUrl1)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
       drawCurrentForecast(data);
       drawFiveDayForecast(data);
-      console.log(apiUrl1)
+      drawSearchHistory(data)
+      console.log(apiUrl1);
+      console.log(apiUrl2);
+      console.log(city.value)
     })
     .catch(function() {
       // catch any errors
@@ -30,8 +39,8 @@ function fiveDayForecast () {
     fetch(apiUrl2)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
-      drawFiveDayWeather(data);
-      console.log(apiUrl2)
+      drawFiveDayForecast(data);
+      
     })
     .catch(function() {
       // catch any errors
@@ -50,8 +59,8 @@ function drawCurrentForecast( d ) {
 	var fahrenheight = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
 	
     document.getElementById('location').innerHTML = "City: " + d.name;
-	document.getElementById('description').innerHTML = d.weather[0].description;
-	document.getElementById('temp').innerHTML = "Temp: " + fahrenheight + '&deg;';
+	  document.getElementById('wind').innerHTML = "Wind Speed: " + d.wind.speed + " MPH";
+	  document.getElementById('temp').innerHTML = "Temp: " + fahrenheight + '&deg;';
     document.getElementById('humidity').innerHTML = "Humidity: " + d.main.humidity + '%';
 }
 
@@ -61,13 +70,44 @@ function drawFiveDayForecast( d ) {
 
 	var fahrenheight = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
 	
-    document.getElementById('DayOne').innerHTML = "Hello"
-	document.getElementById('DayTwo').innerHTML = "Hello"
-	document.getElementById('DayThree').innerHTML = "Hello"
-    document.getElementById('DayFour').innerHTML = "Hello"
-    document.getElementById('DayFive').innerHTML ="Hello"
+    document.getElementById('d1-temp').innerHTML = "Temp: " + fahrenheight + "&deg;"; 
+	  document.getElementById('d1-wind').innerHTML =   "Wind Speed: " + d.wind .speed + " MPH";
+	  document.getElementById('d1-humidity').innerHTML = "Humidity: " + d.main.humidity + "%";
+    document.getElementById('d2-temp').innerHTML = "Temp: " + "&deg;"; 
+	  document.getElementById('d2-wind').innerHTML =   "Wind Speed: " + " MPH";
+	  document.getElementById('d2-humidity').innerHTML = "Humidity: " + "%";
+    document.getElementById('d3-temp').innerHTML = "Temp: " + "&deg;"; 
+	  document.getElementById('d3-wind').innerHTML =   "Wind Speed: " + " MPH";
+	  document.getElementById('d3-humidity').innerHTML = "Humidity: " + "%";
+    document.getElementById('d4-temp').innerHTML = "Temp: " + "&deg;"; 
+	  document.getElementById('d4-wind').innerHTML =   "Wind Speed: " + " MPH";
+	  document.getElementById('d4-humidity').innerHTML = "Humidity: " + "%";
+    document.getElementById('d5-temp').innerHTML = "Temp: " + "&deg;"; 
+	  document.getElementById('d5-wind').innerHTML =   "Wind Speed: " + " MPH";
+	  document.getElementById('d5-humidity').innerHTML = "Humidity: " + "%";
+    
 }
 
+function drawSearchHistory(d) {
+
+  let content = city.value
+  let searchHistory = $(this).children(".search-history").attr("class");
+
+  localStorage.setItem(content, searchHistory)
+
+  for (i = 0; i <= content.length; i++){
+    if (content.length = 0) {
+      document.getElementById("search-history1").innerHTML = "-" + city.value;
+      localStorage.setItem(content, searchHistory)
+    }
+    if (content.length = 1) {
+      $("#search-history1").val(localStorage.getItem("search-history1"));
+      document.getElementById("search-history2").innerHTML = "-" + city.value;
+    }
+  }  
+}
+
+$("#search-history1").val(localStorage.getItem("search-history1"));
 
 searchButton.addEventListener("click", getWeather);
 
